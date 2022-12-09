178bc4a0a27aa9d5558845f3ac99087d72833226
Amir <amirg00@walla.com>
2022-12-10 01:41:52 +0200

    Update monitor sending each 5 seconds a ping req, getting arguments with parser args

--- Monitor.py
+++ Monitor.py
@@ -5,18 +5,22 @@ from scapy.layers.inet import IP, ICMP
 
 def build_ping_packet(src_machine_ip, dst_machine_ip):
     ip = IP(src=src_machine_ip, dst=dst_machine_ip)
-    icmp = ICMP()
+    icmp = ICMP(type=8, code=0)
     return ip / icmp
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-
+    # Source ip: -s x.x.x.x
+    parser.add_argument("-s", "--src", dest="src", help="source ip")
+    # Destination ip: -d x.x.x.x
+    parser.add_argument("-d", "--dest", dest="dest", help="destination ip")
     # -l python/c
     parser.add_argument("-l", "--lang", dest="language", help="current language")
 
     parser_args = parser.parse_args()
-    if parser_args.language.lower() != 'c' or parser_args.language.lower() != 'python':
+    print(parser_args.language == "python ")
+    if parser_args.language.lower() != 'c' and parser_args.language.lower() != 'python':
         print("Bad input for flag language")
         exit(0)
 
@@ -26,32 +30,36 @@ if __name__ == '__main__':
     rtt_accumulator = 0
     pkt_cnt = 0
 
-    args_list = sys.argv[1:]
-    src_machine_ip, server_machine_ip = args_list
+    src_machine_ip = parser_args.src
+    server_machine_ip = parser_args.dest
 
     while True:
-        icmp_pkt = build_ping_packet(src_machine_ip, server_machine_ip)
+        try:
+            icmp_pkt = build_ping_packet(src_machine_ip, server_machine_ip)
 
-        # Save current time before sending
-        start = time.time()
+            # Save current time before sending
+            start = time.time()
+            # Send and receive the ping packet to server
+            sr1(icmp_pkt)
+            # Save current time after receiving
+            finish = time.time()
+            # Calculate the round trip time of the packet.
+            RTT = finish - start
+            rtt_accumulator += RTT
 
-        # Send and receive the ping packet to server
-        sr1(icmp_pkt)
+            # Write result to file and save it in an accumulator variable for total average
+            write_str = f"Total time taken to send and receive ping packet NO.{pkt_cnt} (RTT): {RTT} seconds.\n"
+            ping_result_file.write(write_str)
+            pkt_cnt += 1
 
-        # Save current time after receiving
-        finish = time.time()
+            # Wait for 5 seconds
+            time.sleep(5)
 
-        # Calculate the round trip time of the packet.
-        RTT = finish - start
-        rtt_accumulator += RTT
+        # User has terminated the program so right final bottom line
+        except KeyboardInterrupt:
+            break
 
-        # Write result to file and save it in an accumulator variable for total average
-        write_str = f"Total time taken to send and receive ping packet NO.{pkt_cnt} (RTT): {RTT} seconds.\n"
-        ping_result_file.write(write_str)
-
-        # Wait for 5 seconds
-        time.sleep(5)
-
-    write_final_line = f"{'-' * 70}\nAverage RTT (Round Trip Time): {rtt_accumulator / pkt_cnt}"
-    file.write(write_final_line)
-    file.close()
+    # Write bottom line: RTT average number
+    write_final_line = f"{'-' * 100}\nAverage RTT (Round Trip Time): {rtt_accumulator / pkt_cnt}"
+    ping_result_file.write(write_final_line)
+    ping_result_file.close()

